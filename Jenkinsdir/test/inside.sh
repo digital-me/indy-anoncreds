cff359da9cd8fc713f960b63228248347a8b4951
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-03-03 11:50:57 +0100

    Assume PBC and Charm already installed and point to test dir
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- Jenkinsdir/test/inside.sh
+++ Jenkinsdir/test/inside.sh
@@ -9,9 +9,13 @@ source "${SDIR}/../common.sh"
 # Define global variables
 TESTONLYSLICE='1/1'
 
+echo "Prepare and execute Charm setup script..."
+sed -r 's/sudo//' setup-charm.sh > "${SDIR}/setup-charm.sh"
+
+/bin/bash -e "${SDIR}/setup-charm.sh"
+
 echo "Running Python (v${PYTHON_VER}) testing scripts for ${DIST}:"
 
-while read TESTDIR; do
-	RESFILE="test-result-${TESTDIR#*_}.txt"
-	PYTHONASYNCIODEBUG='0' ${PYTHON} runner.py --pytest "${PYTHON} -m pytest" --dir ${TESTDIR} --output "${RESFILE}" --test-only-slice "${TESTONLYSLICE}"
-done < <(ls -1d indy_*)
+TESTDIR='anoncreds'
+RESFILE="test-result-${TESTDIR#*_}.txt"
+PYTHONASYNCIODEBUG='0' ${PYTHON} runner.py --pytest "${PYTHON} -m pytest" --dir ${TESTDIR} --output "${RESFILE}" --test-only-slice "${TESTONLYSLICE}"
