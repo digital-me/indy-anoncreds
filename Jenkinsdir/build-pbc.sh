714b0364b969dfe1f1ee00bed9891f2a9b5aefe2
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-03-06 17:31:24 +0100

    Install PBC library for other user programs
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- Jenkinsdir/build-pbc.sh
+++ Jenkinsdir/build-pbc.sh
@@ -43,6 +43,7 @@ case "${TARGET}" in
 		./configure --silent
 		make --silent > /dev/null
 		make --silent install > /dev/null
+		[ -d '/usr/lib64' ] && libtool --finish /usr/lib64 || libtool --finish /usr/lib
 	;;
 	rpm)
 		mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,TMP}
