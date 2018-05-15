ba79facfdc87d64e39ab2462c75333f7480d25a8
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-05-15 14:16:33 +0200

    Remove rsync completely
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- lazyDir/publish/repos.sh
+++ lazyDir/publish/repos.sh
@@ -8,9 +8,6 @@ source "${SDIR}/../common.sh"
 
 pushd "${PWD}/dist/${DIST}"
 
-echo "Retrieving old packages for ${DIST} from ${REPO_DEST}:"
-$RSYNC ${DRY_ARG} ${RSYNC_OPTIONS} --ignore-existing "${REPO_DEST}/${DIST}" "dist/${DIST}"
-
 case "${DIST}" in
 	centos*)
 		/usr/bin/createrepo --pretty --compress-type=gz .
@@ -21,8 +18,5 @@ case "${DIST}" in
 	*) # Fall-back, only if called without DIST set
 	;;
 esac
-
-echo "Publishing packages for ${DIST} on ${REPO_DEST}:"
-$RSYNC ${DRY_ARG} ${RSYNC_OPTIONS} --update dist/${DIST} "${REPO_DEST}"
 
 popd
