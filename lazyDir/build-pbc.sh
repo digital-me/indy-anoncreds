4df55d3409a2ee9ee17458ce671a9f8cb5fe6d14
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-05-24 12:34:20 +0200

    Improve support for different types of package to build

--- lazyDir/build-pbc.sh
+++ lazyDir/build-pbc.sh
@@ -3,7 +3,17 @@
 # Define variables and parameters
 : ${TARGET:="${1:-test}"}
 : ${GIT_URL:="${3:-https://github.com/blynn/pbc.git}"}
-: ${DIST:="${LAZY_LABEL:-ubuntu}"}
+: ${DIST:="${LAZY_LABEL}"}
+
+# Try to guess which distro it is if not defined yet
+if [ -z "${DIST}" ]; then
+	if [ -x '/usr/bin/lsb_release' ]; then
+		DIST="$(/usr/bin/lsb_release -si 2> /dev/null | tr '[:upper:]' '[:lower:]' 2> /dev/null)"
+	else
+		# Fallback on the original distro by default
+		DIST="ubuntu"
+	fi
+fi
 
 # Map version to GIT ref if required
 case "$2" in
@@ -28,6 +38,31 @@ fi
 # Save working directory to collect packages later
 WDIR="${PWD}"
 
+# Define packaging steps per type of package
+build_package () {
+		TYPE="${1}"
+		case "${TYPE}" in
+			rpm)
+				mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,TMP}
+				git archive --format=zip --prefix="pbc-${GIT_REF}/" -o "rpmbuild/SOURCES/${GIT_REF}.zip" "${GIT_REF}"
+				cp redhat/libpbc.spec rpmbuild/SPECS
+				pushd rpmbuild
+				/usr/bin/rpmbuild --quiet --define "_topdir ${PWD}" --define "_git_ref ${GIT_REF}" -bb SPECS/libpbc.spec
+				mv RPMS/*/*.rpm "${WDIR}"
+				popd
+			;;
+			deb)
+				echo "Packaging in ${PWD}:"
+				/usr/bin/dpkg-buildpackage -b -uc -us > /dev/null
+				rm -f ../libpbc_*.changes	# Avoid polluting working dir
+				mv ../*.deb "${WDIR}"
+			;;
+			*)
+				echo "Unknown package type (${TYPE})" && exit 1
+			;;
+		esac
+}
+
 # Download source code
 git clone "${GIT_URL}" "${BDIR}"
 # Enter build dir
@@ -49,25 +84,19 @@ case "${TARGET}" in
 		grep -R '/usr/local/lib' /etc/ld.so.conf.d || echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf
 		/sbin/ldconfig
 	;;
+	rpm|deb)
+		build_package "${TARGET}"
+	;;
 	package)
 		case "${DIST}" in
 			centos*)
-				mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,TMP}
-				git archive --format=zip --prefix="pbc-${GIT_REF}/" -o "rpmbuild/SOURCES/${GIT_REF}.zip" "${GIT_REF}"
-				cp redhat/libpbc.spec rpmbuild/SPECS
-				pushd rpmbuild
-				/usr/bin/rpmbuild --quiet --define "_topdir ${PWD}" --define "_git_ref ${GIT_REF}" -bb SPECS/libpbc.spec
-				mv RPMS/*/*.rpm "${WDIR}"
-				popd
+				build_package rpm
 			;;
 			ubuntu*)
-				echo "Packaging in ${PWD}:"
-				/usr/bin/dpkg-buildpackage -b -uc -us > /dev/null
-				rm -f ../libpbc_*.changes	# Avoid polluting working dir
-				mv ../*.deb "${WDIR}"
+				build_package deb
 			;;
-		esac
-	;;	
+	esac
+	;;
 	*)
 		echo "Unknown TARGET (${TARGET})" && exit 1
 	;;
